b7c1bf2a96a03b8c83b3cabdeb2c55125c761b19
Sandroda9 <danner.sandro@bluewin.ch>
2024-11-19 17:47:45 -0600

    FacebookTest_Sandro

---
+++ src/test/java/com/mayab/quality/functional/FacebookTest.java
@@ -0,0 +1,92 @@
+package com.mayab.quality.functional;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.time.Duration;
+public class FacebookTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+    WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+    baseUrl = "https://www.facebook.com/";
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+    js = (JavascriptExecutor) driver;
+  }
+  @Test
+  public void testWrongLogin() throws Exception {
+    driver.get("https://www.facebook.com/");
+    pause(3000);
+    driver.findElement(By.id("email")).clear();
+    driver.findElement(By.id("email")).sendKeys("house");  
+    pause(3000);
+    driver.findElement(By.id("pass")).clear();
+    driver.findElement(By.id("pass")).sendKeys("house"); 
+    pause(3000);
+    driver.findElement(By.name("login")).click();
+    pause(3000);
+    
+    String actualResult = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div/div[2]/div[2]/form/div/div[2]/div[2]")).getText();
+   
+    assertTrue(actualResult.contains("Das von dir eingegebene Passwort ist falsch."));
+      
+  }
+  
+  private void pause(long mils) {
+	  try {
+		  Thread.sleep(mils);
+		  
+	  }catch(Exception e) {
+		  e.printStackTrace();
+	  }
+  }
+  @After
+  public void tearDown() throws Exception {
+    //driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
